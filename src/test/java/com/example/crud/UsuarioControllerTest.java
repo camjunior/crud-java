baa2f72fe4699ea0aba0101d41cb90f7a14b7d4e
camjunior <camjunior90@gmail.com>
2023-08-02 11:53:07 -0300

    Adicionando testes

--- src/test/java/com/example/crud/UsuarioControllerTest.java
+++ src/test/java/com/example/crud/UsuarioControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.crud.controller;
+package com.example.crud;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -9,7 +9,7 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,9 +18,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.example.crud.entity.Usuario;
 import com.example.crud.repository.UsuarioRepository;
+import com.example.crud.controller.UsuarioController;
 
-@WebMvcTest(UsuarioController.class)
+@SpringBootTest
 @AutoConfigureMockMvc
+
 public class UsuarioControllerTest {
 
     @Autowired
